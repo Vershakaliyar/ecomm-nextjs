bfe8b95cb3e66213dcb3decf010ea1ae57f65395
Vershakaliyar <90090251+Vershakaliyar@users.noreply.github.com>
2022-07-06 16:30:31 +0530

    Update baseUrl.js

--- helpers/baseUrl.js
+++ helpers/baseUrl.js
@@ -1,2 +1,2 @@
-const baseUrl = process.env.NODE_ENV === 'production' ? "https://onlinestorenext.vercel.app": "http://localhost:3000"
+const baseUrl = process.env.NODE_ENV === 'production' ? "ecomm-nextjs.vercel.app": "http://localhost:3000"
 export default baseUrl
